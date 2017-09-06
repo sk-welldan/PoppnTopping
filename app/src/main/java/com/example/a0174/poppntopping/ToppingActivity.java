a483bc12ac4df154301c164340228272ac1ddb0c
sk-welldan <sk.welldan1126@gmail.com>
2017-09-06 18:43:14 +0900

    initial

---
+++ app/src/main/java/com/example/a0174/poppntopping/ToppingActivity.java
@@ -0,0 +1,13 @@
+package com.example.a0174.poppntopping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topping);
+    }
+}
